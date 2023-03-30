90a98a9e04b5be06eca29ae832170fd602077fd1
K3A101 <keisha.alexander@hva.nl>
2023-03-30 16:51:37 +0200

    De data wordt opgeslagen in de formulier

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,20 @@ const formInputs = document.querySelectorAll('input[type="text"]', 'input[type="
 function saveFormInputsToLocal() {
     // Loop over each form input and save its value to local storage
     formInputs.forEach(input => {
-        const inputName = input.name; // Get the name of the input element
-        const inputValue = input.value; // Get the current value of the input element
+        let inputName = input.name; // Get the name of the input element
+        let inputValue = input.value; // Get the current value of the input element
         localStorage.setItem(inputName, inputValue); // Save the input value in local storage
+       
+    });
+}
+
+function loadFormInputToLocal() {
+    formInputs.forEach(input => {
+        const inputName = input.name;
+        let storedValue = localStorage.getItem(inputName);
+        if(storedValue){
+            input.value = storedValue
+        }
     });
 }
 
@@ -16,4 +27,4 @@ formInputs.forEach(input => {
 });
 
 // Call the function once on page load to save initial values
-saveFormInputsToLocal();
+loadFormInputToLocal();
